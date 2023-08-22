d8ae5b6b28a530278e1b422835c5b04a8daecc8c
shivamupadhayay07 <upadhayayshivam0717@gmail.com>
2023-08-22 17:24:09 +0530

    commit

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,7 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div> <imgCard/> </div>
+    <div className='container'>
+    <h1>hello world </h1>
+    <div className='row'>
+    {data1.map((card, index)=>{
+      <imgCard
+      title = {card.title}
+      desc={card.desc}
+      ingurl={card.ingurl}
+      />
+    })}
+    </div>
+    
+    </div>
    
   );
 }
