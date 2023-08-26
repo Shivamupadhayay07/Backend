6e353d24aff7469046a625278e4d423cba87e133
shivamupadhayay07 <upadhayayshivam0717@gmail.com>
2023-08-26 17:36:12 +0530

    commits

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -3,26 +3,45 @@ import './App.css';
 import NavBar from './components/NavBar';
 import HomeCarousel from './components/HomeCarousel';
 import ImgCard from './components/ImgCard';
+import { useEffect } from 'react';
+import { useState } from 'react';
 
-import data from './data';
-
-
-
-
+// import data from './data';
 
 
 function App() {
+
+  const [Data, setData]= useState([]);
+  useEffect(()=>{
+    const fetchdata = async () => {
+      try{
+        const response = await fetch ("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setData(data);
+
+        // const dataArray = Object.keys(fetchdata).map ((id)=>({
+        //   id,
+        //   fetchdata[id],
+        // }));
+
+
+      }
+      catch(error){
+        console.log("Error occured", error);
+      }
+}; fetchdata();
+  },[]);
   return (
     <div>
     <NavBar/> 
     <HomeCarousel/>
     <div className='img1'>
     {
-      data.map((obj)=>{
+      Data.map((obj)=>{
         
         return(
         <div key={obj.id}>
-         <ImgCard title={obj.title} desc={obj.desc} imgurl={obj.imgurl}/>
+         <ImgCard title={obj.title} desc={obj.desc} image={obj.image}/>
         </div> )      
       })
     }      
--- myapp/src/components/ImgCard.jsx
+++ myapp/src/components/ImgCard.jsx
@@ -16,7 +16,7 @@ function ImgCard(props) {
   return (
     <div>
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={props.imgurl} />
+      <Card.Img variant="top" src={props.image} />
       <Card.Body>
       {props.id}
         <Card.Title>{props.title}</Card.Title>
